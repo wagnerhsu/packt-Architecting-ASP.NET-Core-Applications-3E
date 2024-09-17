3c2575ceae71a1990c132baa3f56f0b2c1f429cb
Wagner Xu <wagnerhsu@hotmail.com>
2024-09-17 16:18:18 +0800

    feat: add SpecialType

--- C02/test/MyApp.IntegrationTests/Controllers/HelloWorldControllerTest.cs
+++ C02/test/MyApp.IntegrationTests/Controllers/HelloWorldControllerTest.cs
@@ -1,27 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
-using System.Net;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace MyApp.IntegrationTests.Controllers;
 
 public class HelloWorldControllerTest : IClassFixture<WebApplicationFactory<Startup>>
 {
     private readonly HttpClient _httpClient;
+
     public HelloWorldControllerTest(WebApplicationFactory<Startup> webApplicationFactory)
     {
         _httpClient = webApplicationFactory.CreateClient();
     }
+
     public class Hello : HelloWorldControllerTest
     {
-        public Hello(WebApplicationFactory<Startup> webApplicationFactory)
-            : base(webApplicationFactory) { }
+        private readonly ITestOutputHelper _logger;
+
+        public Hello(WebApplicationFactory<Startup> webApplicationFactory, ITestOutputHelper logger)
+            : base(webApplicationFactory)
+        {
+            _logger = logger;
+        }
 
         [Fact]
         public async Task Should_respond_a_status_200_OK()
         {
             // Act
             var result = await _httpClient.GetAsync("/");
-
+            var data = await result.Content.ReadAsStringAsync();
+            _logger.WriteLine($"GetData:{data}");
             // Assert
             Assert.Equal(HttpStatusCode.OK, result.StatusCode);
         }
